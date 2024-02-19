eb9a64f76810d43cc8cd09f5f6e9d9742da9451d
Sadia Khan <khansadia220@gmail.com>
2024-02-19 21:52:53 +0100

    Added Navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,9 @@
+import "./App.css";
+import Navbar from "./Navbar";
 export function App() {
   return (
     <>
+      <Navbar />
       <h1>This is the App Component</h1>
     </>
   );
---
+++ src/Navbar.jsx
@@ -0,0 +1,13 @@
+function Navbar(){
+    return(
+        <nav>
+            <a href="#hot">Hot</a>
+            <a href="#Juicy">Juicy</a>
+        
+            <a href="#cosy">Cosy</a>
+        
+
+        </nav>
+    );
+}
+export default Navbar;
